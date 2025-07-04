6e388f7bba4dc734be48aa817b30a4dfd8a6c083
phnguynmkhoi <pnmk0811@gmail.com>
2025-07-05 02:16:39 +0700

    small fix

--- core_ai/libs/agents/outline_agent/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Outline Agent module for the Tree of Thought slide generation system.
-This agent is responsible for creating presentation outlines.
-"""
-
-from libs.agents.outline_agent.agent import OutlineAgent
-
-__all__ = ["OutlineAgent"]
--- core_ai/libs/agents/svg_diagram_agent/__init__.py
+++
@@ -1,3 +0,0 @@
-from .agent import SVGDiagramAgent
-
-__all__ = ["SVGDiagramAgent"]
